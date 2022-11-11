6c31f88c6c480a9f833630678ff9f386e588ab8c
Keiran Raine <keiran.raine@eahsn.org>
2022-11-11 15:04:54 +0000

    fixes up hubflow and precommit

--- .devcontainer/extra.sh
+++ .devcontainer/extra.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-set -u
+set -uxe
+set -o pipefail
 
+pip install --upgrade pip
+
+## global install of pre-commit
+pip install pre-commit
+pre-commit install --install-hooks
+
+cd /tmp && sudo rm -rf gitflow && git clone https://github.com/datasift/gitflow && cd gitflow && sudo ./install.sh && sudo git hf upgrade
+
+set +e
 hash nextflow >& /dev/null || (
     cd /tmp
     curl -s https://get.nextflow.io | bash
