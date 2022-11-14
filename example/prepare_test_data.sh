9a180810c9b6e36212a3d75ccaeda85ab37caab0
Keiran Raine <keiran.raine@eahsn.org>
2022-11-14 13:07:04 +0000

    use appropriate user/group to ensure permissions are okay

--- example/prepare_test_data.sh
+++ example/prepare_test_data.sh
@@ -14,9 +14,10 @@ curl -sSLO https://github.com/Illumina/ExpansionHunter/raw/${TAG}/example/input/
 curl -sSLO https://github.com/Illumina/ExpansionHunter/raw/${TAG}/example/input/variants.json
 
 # build some extra indicies to prove multiple routes, the expansion hunter image works well for this
+docker pull quay.io/wtsicgp/expansion_hunter:5.0.0
 
 ## adds csi index to files
-docker run -v $PWD:/d:rw --rm quay.io/wtsicgp/expansion_hunter:5.0.0 samtools index -c /d/variants.bam
+docker run $(id -u ${USER}):$(id -g ${USER}) -v $PWD:/d:rw --rm quay.io/wtsicgp/expansion_hunter:5.0.0 samtools index -c /d/variants.bam
 ## create cram input
-docker run -v $PWD:/d:rw --rm quay.io/wtsicgp/expansion_hunter:5.0.0 samtools view -T /d/reference.fa -C -o /d/variants.cram /d/variants.bam
-docker run -v $PWD:/d:rw --rm quay.io/wtsicgp/expansion_hunter:5.0.0 samtools index /d/variants.cram
+docker run $(id -u ${USER}):$(id -g ${USER}) -v $PWD:/d:rw --rm quay.io/wtsicgp/expansion_hunter:5.0.0 samtools view -T /d/reference.fa -C -o /d/variants.cram /d/variants.bam
+docker run $(id -u ${USER}):$(id -g ${USER}) -v $PWD:/d:rw --rm quay.io/wtsicgp/expansion_hunter:5.0.0 samtools index /d/variants.cram
