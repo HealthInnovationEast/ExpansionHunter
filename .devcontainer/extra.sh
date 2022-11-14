8530780d0e1185cb99ae59b85464bcfc4de240ef
Keiran Raine <keiran.raine@eahsn.org>
2022-11-14 15:11:04 +0000

    fix up extra

--- .devcontainer/extra.sh
+++ .devcontainer/extra.sh
@@ -9,12 +9,18 @@ pip install --upgrade pip
 pip install pre-commit
 pre-commit install --install-hooks
 
-cd /tmp && sudo rm -rf gitflow && git clone https://github.com/datasift/gitflow && cd gitflow && sudo ./install.sh && sudo git hf upgrade
+(cd /tmp && sudo rm -rf gitflow && git clone https://github.com/datasift/gitflow && cd gitflow && sudo ./install.sh && sudo git hf upgrade)
+sudo rm -rf /tmp/gitflow
+
 
 set +e
+# this fails if unstaged changes, however they should only exist when resuming
+git hf init -f
 hash nextflow >& /dev/null || (
     cd /tmp
     curl -s https://get.nextflow.io | bash
     mkdir -p $HOME/.local/bin
     mv nextflow $HOME/.local/bin/
 )
+
+exit 1
